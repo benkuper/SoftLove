3cb2f612bfc7854bd28f0385fe9969618bfc14b6
mdeveaux <mariondeveaux@gmail.com>
2015-11-23 12:22:47 +0100

    modfication clé api google

--- Web/src/main/java/src/MainWeb.java
+++ Web/src/main/java/src/MainWeb.java
@@ -1,31 +1,37 @@
 package src;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.json.simple.parser.ParseException;
 
 public class MainWeb {
 
 	public static String profil = "animal";
+	public static String googleKey;
 	
 	public static void main(String[] args) throws IOException, ParseException {
 		
 		String recherche ="cheval blanc";
 		new ZMQConnector();
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir la clé de google API :");
+		googleKey = sc.nextLine();
+		
 		System.out.println("lancement thread profil");
 		ProfilThread thp = new ProfilThread();
 		thp.start();
 		
-		//while(true) {
+		while(true) {
 
 			System.out.println("begin loop");
 			
-			//recherche = ZMQConnector.receiveKeywords();
+			recherche = ZMQConnector.receiveKeywords();
 			System.out.println("recherche : " + recherche+"\t"+"profil : "+profil);
 		
 			MainThread t = new MainThread(recherche, profil);
 			t.start();
-		//}
+		}
 	}
 }
