1dd7585161ab6eff0eba349b779b95f0d7b91b6a
Ben Kuper <bkuperberg@gmail.com>
2015-11-28 23:45:15 +0100

    clean emotion

--- Emotion/workspace/src/main/java/fr/insa/ihme/emotion/publisher/PublisherEmotion.java
+++
@@ -1,7 +0,0 @@
-package fr.insa.ihme.emotion.publisher;
-
-public interface PublisherEmotion {
-	public void init(int ioThreads, String addr);
-	public void publish(String topic, String content);
-	public void close();
-}
