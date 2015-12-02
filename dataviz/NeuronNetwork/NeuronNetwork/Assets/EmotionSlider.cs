2d0f9815152e366087abe582ad8bb8563a20dd82
Ben Kuper <bkuperberg@gmail.com>
2015-12-02 11:50:53 +0100

    Added emotionSlider

---
+++ dataviz/NeuronNetwork/NeuronNetwork/Assets/EmotionSlider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class EmotionSlider : MonoBehaviour {
+
+    Transform bar;
+    public string type;
+
+	// Use this for initialization
+	void Start () {
+        bar = transform.FindChild("bg").FindChild("bar");
+        setValue(.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setValue(float value)
+    {
+        bar.DOScaleX(value*.99f,.3f);
+    }
+}
