2c041b7d7bbd8b10383211d4ce5461895d904954
fhardy <flavien.hardy@insa-rouen.fr>
2015-12-07 11:54:02 +0100

    Ajout des fichiers de detection de geste

---
+++ Tracking/src/SoftLove/GestureBehaviour/Gestures/HandsTogetherGesture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace SoftLove.GestureBehaviour.Gestures
+{
+    class HandsTogetherGesture : IGestureRecognizer
+    {
+        /// <summary>
+        /// Information à renvoyer
+        /// </summary>
+        /// <returns></returns>
+        public string GetInfo()
+        {
+            return "MainsJointes";
+        }
+
+        /// <summary>
+        /// Fonction de reconnaissance du geste "Mains Jointes"
+        /// </summary>
+        /// <param name="skeletonData"></param>
+        /// <returns></returns>
+        public bool IsRecognized(Skeleton[] skeleton)
+        {
+            try
+            {
+                if (skeleton.Length > 0)
+                {
+                    var skel = skeleton.Where(u => u.TrackingState == SkeletonTrackingState.Tracked).FirstOrDefault();
+                    if (skel != null) {
+                        return (Math.Abs(
+                            skel.Joints[JointType.HandRight].Position.Y -
+                            skel.Joints[JointType.HandLeft].Position.Y) +
+                            Math.Abs(skel.Joints[JointType.HandRight].Position.X -
+                            skel.Joints[JointType.HandLeft].Position.X)) < 0.04;
+                    }
+                }
+                return false;
+            }
+            catch (NullReferenceException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Tracking/src/SoftLove/GestureBehaviour/Gestures/HelloGesture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace SoftLove.GestureBehaviour.Gestures
+{
+    class HelloGesture : IGestureRecognizer
+    {
+        /// <summary>
+        /// Informations à renvoyer
+        /// </summary>
+        /// <returns></returns>
+        public string GetInfo()
+        {
+            return "hello";
+        }
+
+        public bool IsRecognized(Skeleton[] skeleton)
+        {
+            try
+            {
+                if (skeleton.Length > 0)
+                {
+                    var skel = skeleton.Where(u => u.TrackingState == SkeletonTrackingState.Tracked).FirstOrDefault();
+                    if (skel != null)
+                    {
+                        return ((skel.Joints[JointType.HandRight].Position.Y -
+                                skel.Joints[JointType.Head].Position.Y) > 0)
+                                || ((skel.Joints[JointType.HandLeft].Position.Y -
+                                skel.Joints[JointType.Head].Position.Y) > 0)
+                                ;
+                    }
+                }
+                return false;
+            }
+            catch (NullReferenceException)
+            {
+                return false;
+            }
+        }
+    }
+}
